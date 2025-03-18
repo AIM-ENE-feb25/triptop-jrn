20fbeba725f9fb923f95f86addb9ef8ed83a1009
Rob Kokx <150233960+RobKokx@users.noreply.github.com>
2025-03-18 14:42:51 +0100

    huiswerk mappie

---
+++ huiswerkAPI/src/main/java/com/example/huiswerkAPI/HuiswerkApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.huiswerkAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuiswerkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuiswerkApiApplication.class, args);
+	}
+
+}
---
+++ huiswerkAPI/src/test/java/com/example/huiswerkAPI/HuiswerkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.huiswerkAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuiswerkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
