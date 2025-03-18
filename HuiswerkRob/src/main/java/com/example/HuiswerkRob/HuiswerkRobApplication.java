8a5f6b12ff50ff82c521cddb4970e85a6e79cdd5
Pytugar <150233960+RobKokx@users.noreply.github.com>
2025-03-19 00:58:01 +0100

    .

---
+++ HuiswerkRob/src/main/java/com/example/HuiswerkRob/HuiswerkRobApplication.java
@@ -0,0 +1,13 @@
+package com.example.HuiswerkRob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuiswerkRobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuiswerkRobApplication.class, args);
+	}
+
+}
