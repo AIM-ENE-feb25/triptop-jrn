dc9dc8fe766ea3906b6c58966c76f4451c54d40d
Jae Dreijling <j.dreijling@student.han.nl>
2025-03-21 09:38:59 +0100

    setup workspace

---
+++ huiswerkJae/demo/src/main/java/TripTropExperiment/demo/TripTropApplication.java
@@ -0,0 +1,13 @@
+package TripTropExperiment.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripTropApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripTropApplication.class, args);
+	}
+
+}
---
+++ huiswerkJae/demo/src/test/java/TripTropExperiment/demo/TripTropApplicationTests.java
@@ -0,0 +1,13 @@
+package TripTropExperiment.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripTropApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
