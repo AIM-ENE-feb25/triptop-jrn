6991af66b8d1296b0169796500e0a45e4654ea6b
Osomizu <jae.dreijlingg@outlook.com>
2025-04-01 20:11:48 +0200

    setup environment

---
+++ huiswerkJae/SOEX_PROJECT_JRN_Jae/src/main/java/com/example/SOEX_PROJECT_JRN/SoexProjectJrnApplication.java
@@ -0,0 +1,13 @@
+package com.example.SOEX_PROJECT_JRN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoexProjectJrnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoexProjectJrnApplication.class, args);
+	}
+
+}
